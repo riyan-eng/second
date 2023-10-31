5089912a18a4d6cd2f0ae5254080c038b9982278
riyan-eng <fbriyant11@gmail.com>
2023-10-31 15:05:17 +0700

    first commit

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, Second World!")
+	})
+
+	app.Get("/:name", func(c *fiber.Ctx) error {
+		str := fmt.Sprintf(`Hello, %v`, c.Params("name"))
+		return c.SendString(str)
 	})
 
 	app.Listen(":3000")
